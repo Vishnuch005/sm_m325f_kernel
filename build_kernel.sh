c2b2340f47ac565a130241f339ec9314142bd612
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-02-08 23:07:58 +0530

    gitignore: don't ignore .patch file
    include patch file in commit
    
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export CROSS_COMPILE=/home/portablev/projects/aarch64-4.9/bin/aarch64-linux-android-
+export CC=/home/portablev/projects/linux-x86-3857008389202edac32d57008bb8c99d2c957f9d-clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
@@ -10,6 +10,6 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y m32_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j4
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
